dda3619df2ec5f473a5fa4312b6b7a06ae8c5f1b
Dustin <dmkelly100@gmail.com>
2020-04-14 15:28:48 -0400

    removed folder

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,31 @@
-console.log('it works')
+// Name Space
+const plantApp = {};
+
+plantApp.apiToken = 'Wm9LK3NsZHhXQWFRSTFJdGxidStjZz09';
+plantApp.baseURL = 'http://trefle.io/api/plants';
+
+plantApp.retrieveData = () => {
+	$.ajax({
+		url: 'http://proxy.hackeryou.com',
+		method: 'GET',
+		dataType: 'json',
+		data: {
+			reqUrl: plantApp.baseURL,
+			params: {
+				token: plantApp.apiToken,
+				complete_data: true,
+			},
+		},
+	}).then((request) => {
+		console.log(request);
+	});
+};
+
+plantApp.init = () => {
+	plantApp.retrieveData();
+};
+
+// Doc Ready
+$(function () {
+	plantApp.init();
+});
