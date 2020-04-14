90ab0b69ca35a26c9ad38b2846e400d87c925be5
Dustin <dmkelly100@gmail.com>
2020-04-14 17:40:19 -0400

    added api together

--- scripts/script.js
+++ scripts/script.js
@@ -2,22 +2,32 @@
 const plantApp = {};
 
 plantApp.apiToken = 'Wm9LK3NsZHhXQWFRSTFJdGxidStjZz09';
-plantApp.baseURL = 'http://trefle.io/api/plants';
+plantApp.baseUrl = `http://trefle.io/api/plants`;
 
 plantApp.retrieveData = () => {
 	$.ajax({
-		url: 'http://proxy.hackeryou.com',
-		method: 'GET',
-		dataType: 'json',
+		url: `http://proxy.hackeryou.com`,
+		method: `GET`,
+		dataType: `json`,
 		data: {
-			reqUrl: plantApp.baseURL,
+			reqUrl: plantApp.baseUrl,
 			params: {
 				token: plantApp.apiToken,
-				complete_data: true,
+				format: `json`,
+				q: `apple`,
 			},
 		},
-	}).then((request) => {
-		console.log(request);
+	}).then((data) => {
+		console.log(data);
+		plantApp.firstCall(data);
+	});
+};
+
+plantApp.firstCall = (data) => {
+	plantApp.linkData = data;
+
+	plantApp.linkData.forEach((data) => {
+		console.log(data.scientific_name, data.link, data.id);
 	});
 };
 
@@ -25,7 +35,46 @@ plantApp.init = () => {
 	plantApp.retrieveData();
 };
 
-// Doc Ready
 $(function () {
 	plantApp.init();
 });
+
+//     }).then((result) => {
+//         const artArray = result.artObjects;
+//         // console.log(artArray);
+
+//         artApp.displayArt(artArray);
+//     })
+// }
+
+// // We want to search for artworks related to Monkeys
+
+// // Display the results onto the page
+// artApp.displayArt = (artArray) => {
+//     artArray.forEach((art) => {
+//         const title = art.title;
+//         const author = art.principalOrFirstMaker;
+//         const image = art.webImage.url;
+
+//         console.log(title, author, image);
+
+//         const artEl = `
+//             <div class="piece">
+//                 <h2>${title}</h2>
+//                 <p>${author}</p>
+//                 <img src="${image}" alt="${title}">
+//             </div>
+//         `
+//         $('#artwork').append(artEl);
+//     })
+// }
+
+//  const animals = ['dolphin', 'butterfly', 'otter'];
+// const pluralAnimals = animals.map((animal,index) => {
+//         // return `${animals}s`
+//         return {
+//             animalType: animal,
+//             key: index,
+//         };
+//     });
+//     console.log(pluralAnimals);
