8116683ee8c5eb6eb8050bd9b92f0c46aeba534e
Dustin <dmkelly100@gmail.com>
2020-04-21 09:59:46 -0400

    replaced nulls with Unknown

--- scripts/script.js
+++ scripts/script.js
@@ -20,7 +20,6 @@ plantApp.retrieveData = () => {
 			},
 		},
 	}).then((request) => {
-		console.log(request);
 		plantApp.firstCall(request);
 	});
 };
@@ -55,7 +54,6 @@ plantApp.secondCall = () => {
 			},
 		}).catch((e) => null);
 	});
-	console.log(plantApp.storedPromises);
 	plantApp.displayContentToPage();
 };
 
@@ -65,14 +63,21 @@ plantApp.displayContentToPage = () => {
 	all
 		.then((getValues) => {
 			const justTheGoodStuff = getValues.filter((items) => items != null);
-			console.log(justTheGoodStuff, 'good stuff');
 
 			justTheGoodStuff.forEach((plantObject) => {
+				let moistureUse = plantApp.checkNull(
+					plantObject.main_species.growth.moisture_use
+				);
+				let fire = plantApp.checkNull(
+					plantObject.main_species.growth.fire_tolerance
+				);
+
 				let plantImage = plantObject.images;
 				// checks if there are any images in the array
 				plantImage.length > 0
 					? (plantImage = plantObject.images[0].url)
 					: (plantImage = '.../../imgs/missingImage.jpg');
+
 				const htmlBox = `
 			<div class="plantsInfoBox" tabindex="0">
 				<div class=topText>
@@ -83,8 +88,8 @@ plantApp.displayContentToPage = () => {
 					<ul>
                         <li>More Info: <span><a href="${plantObject.main_species.sources[0].source_url}">${plantObject.main_species.sources[0].source_url}</a></span></li>
 						<li>Native Statues: <span>${plantObject.native_status}</span></li>
-						<li>Fire Tolerance: <span>${plantObject.main_species.growth.fire_tolerance}</span></li>
-						<li>Moisture Use: <span>${plantObject.main_species.growth.moisture_use}</span></li>
+						<li>Fire Tolerance: <span>${fire}</span></li>
+						<li>Moisture Use: <span>${moistureUse}</span></li>
                     </ul>
 				</div>
 			`;
@@ -97,6 +102,16 @@ plantApp.displayContentToPage = () => {
 			$('.animeText').text(`Something Went Wrong, Try Again`).show();
 		});
 };
+
+// Checks for nulls and replaces the value with Unknown
+plantApp.checkNull = (str) => {
+	if (str) {
+		return str;
+	} else {
+		return 'Unknown';
+	}
+};
+
 plantApp.animation = () => {
 	const textWrapper = document.querySelector('p');
 	textWrapper.innerHTML = textWrapper.textContent.replace(
@@ -159,14 +174,6 @@ plantApp.displayLoadingScreen = () => {
 	});
 };
 
-plantApp.checkForNull = (value) => {
-	if (value === null) {
-		value = 'Unknown';
-	} else {
-		value = value;
-	}
-};
-
 plantApp.init = () => {
 	plantApp.search();
 	plantApp.displayLoadingScreen();
@@ -176,28 +183,3 @@ plantApp.init = () => {
 $(function () {
 	plantApp.init();
 });
-
-// $(function () {
-// 	// Create an array of URLs
-// 	let items = [
-// 		'https://pokeapi.co/api/v2/pokemon/2/',
-// 		'https://pokeapi.co/api/v2/pokemon/ditto/',
-// 		'https://pokeapi.co/api/v2/pokemon/badbadbadbaddddddd/',
-// 	];
-// 	// Create a map of promises and return null if there is an error with any of them.
-// 	let promises = items.map((url) => {
-// 		return $.ajax(url).catch((e) => null);
-// 	});
-// 	// Use Promise.all to wait for all of them to resolve, there will be no errors
-// 	// because we are essentially 'suppressing them' in the above code
-// 	// we immediately call
-// 	let all = Promise.all(promises);
-// 	all
-// 		.then((data) => {
-// 			// these data will include 'null' for any calls that have failed,
-// 			console.log(data, 'data?');
-// 		})
-// 		.catch((e) => {
-// 			console.log(e);
-// 		});
-// });
